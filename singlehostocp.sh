037509c345ee67425e6efd5f50a9679f6ea728cb
prasenforu <prasenforu@hotmail.com>
2019-07-06 08:50:23 +0530

    Update singlehostocp.sh

--- singlehostocp.sh
+++ singlehostocp.sh
@@ -105,19 +105,4 @@ echo "STEP-7 Start Openshift Installation .."
 echo "Make sure Prerequistics runs without error ... If yes then run below command .."
 echo "ansible-playbook -i /root/openshift-one-in-all/allinonehost /root/openshift-ansible/playbooks/deploy_cluster.yml"
 
-### RUN Post installation step
-
-echo "STEP-8 Running Post installation steps .."
-
-cp /etc/origin/master/master-config.yaml /etc/origin/master/master-config.yaml.original
-htpasswd -b -c /etc/origin/master/htpasswd $USER1 $USER12675
-htpasswd -b /etc/origin/master/htpasswd $USER2 $USER22675
-
-#### Providing admin rights to users
-
-oc adm policy add-cluster-role-to-user cluster-admin $USER1
-oc adm policy add-cluster-role-to-user cluster-admin $USER2
-oc adm policy add-scc-to-user privileged $USER1
-oc adm policy add-scc-to-user privileged $USER2
-
 ###### Enjoy use Openshift #######
